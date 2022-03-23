87089b39942a3d9740c6364a8cee3e1a0f2d347c
binhvvdev <vbinh912@gmail.com>
2022-03-23 13:15:40 +0700

    Initial version of SpringDemo generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/ip/springdemo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ip.springdemo.repository;
+
+import com.ip.springdemo.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/ip/springdemo/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.ip.springdemo.security;
+
+import com.ip.springdemo.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
